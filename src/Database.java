1eae343ae1ca0ad493440b96c79b48d49d3c2137
federicomarra <fecrom@gmail.com>
2023-09-28 14:47:01 +0200

    Create Database.java

---
+++ src/Database.java
@@ -0,0 +1,9 @@
+import handheldTracker.BasalProfile;
+import handheldTracker.BolusDelivery;
+
+public class Database {
+    public BasalProfile basalProfile;
+    public BolusDelivery[] bolusDeliveries;
+    public Mesurament[] mesuraments;
+
+}

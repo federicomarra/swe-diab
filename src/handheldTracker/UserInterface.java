9605d6ef7dfb8c5ef568f170db2579ce4124af59
federicomarra <fecrom@gmail.com>
2023-10-06 13:34:18 +0200

    Update UserInterface.java

--- src/handheldTracker/UserInterface.java
+++ src/handheldTracker/UserInterface.java
@@ -11,8 +11,24 @@ public class UserInterface {
         localDatabase = new LocalDatabase();
     }
 
-    public void addBolus(float units, LocalTime time, int delay, BolusMode mode) {
+    public void newBolus(float units, LocalTime time, int delay, BolusMode mode) {
+        switch (mode) {
+            case STANDARD:
+                localDatabase.computeAndInject(carb, mode);
+                break;
+            case EXTENDED:
+                wait(delay*1000*60);    //delay in minutes
+                localDatabase.computeAndInject(carb, mode);
+                break;
+            case MANUAL:
+                addBolus(new BolusDelivery(units, time, delay, mode));
+                break;
+            case PEN:
+                addBolus(new BolusDelivery(units, time, delay, mode));
+                break;
+        }
         //TODO: implement
+        localDatabase.addBolus(new BolusDelivery(units, time, delay, mode));
     }
 
     public void updateBasalProfile(float units, int hour) {

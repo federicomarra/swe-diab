21c31fea4e2fe3337eedf5d85b6a6dc6ff2d2072
federicomarra <fecrom@gmail.com>
2023-10-12 16:46:41 +0200

    calculateResidualUnits() implemented

--- src/handheldTracker/BolusDelivery.java
+++ src/handheldTracker/BolusDelivery.java
@@ -1,24 +1,31 @@
 package handheldTracker;
 
+import java.time.Duration;
 import java.time.LocalTime;
+import java.util.List;
 
 public class BolusDelivery {
     public float units;
     public LocalTime time;
-    public int delay;
     public BolusMode mode;
 
-    BolusDelivery(float units, LocalTime time, int delay, BolusMode mode) {
+    BolusDelivery(float units, LocalTime time, BolusMode mode) {
         this.units = units;
         this.time = time;
-        this.delay = delay;
         this.mode = mode;
     }
 
-    public float calculateResidualUnits() {
-        //TODO: implement
-        return 0;
+    public float calculateResidualUnits(List<BolusDelivery> bds) {  // bds = bolusDeliveries
+        int i = 0;
+        Duration diff;
+        float residualUnits = 0;
+        do {    // calculate residual units from last 3 hours (linear decay)
+            i++;
+            BolusDelivery lb = bds.get(bds.size()-i);  // last bolus
+            diff = Duration.between(lb.time, LocalTime.now());
+            residualUnits += lb.units*(1 - (float)diff.toMinutes()/180);
+        } while (diff.toMinutes() <= 180 && i < bds.size());
+        return residualUnits;
     }
-
 
 }

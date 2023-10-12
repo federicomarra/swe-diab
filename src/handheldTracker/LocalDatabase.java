1498bffb3608e42a2ff0d5f2ca4b207f45af6452
federicomarra <fecrom@gmail.com>
2023-10-12 16:46:48 +0200

    Update LocalDatabase.java

--- src/handheldTracker/LocalDatabase.java
+++ src/handheldTracker/LocalDatabase.java
@@ -17,8 +17,9 @@ public class LocalDatabase extends Database implements Observer {
         this.manager = manager;
         this.backupDb = new BackupDatabase();
     }
-    public void update(List<Mesurament> m) {
+    public void update(List<Measurement> ms) {
         try {
+            super.update(ms);
             backup();
         } catch (Exception e) {
             e.printStackTrace();
@@ -26,10 +27,10 @@ public class LocalDatabase extends Database implements Observer {
     }
     public void computeAndInject(int carb, BolusMode mode) throws BluetoothException {
         try {
-            Mesurament lm = mesuraments.get(mesuraments.size()-1);  // last mesurament
+            Measurement lm = measurements.get(measurements.size()-1);  // last mesurament
             Duration diff = Duration.between(lm.time(), LocalTime.now());
-            if (diff.toMinutes() > 10)
-                lm = manager.newMesurament();
+            if (diff.toMinutes() > 10)  // if last mesurament is older than 10 minutes
+                lm = manager.newMesurament();   // get new mesurament
             HourlyFactor sensitivity = insulinSensitivityProfile.hourlyFactors[LocalTime.now().getHour()];
             HourlyFactor carbRatio = carbRatioProfile.hourlyFactors[LocalTime.now().getHour()];
             float units = 0;
@@ -38,11 +39,11 @@ public class LocalDatabase extends Database implements Observer {
                 units += ((float)(lm.glycemia() - 120)) / sensitivity.units();
             if (carb > 0 && carb <= 150)
                 units += carb / carbRatio.units();
-            addBolus(new BolusDelivery(units, LocalTime.now(), 0, mode));
+            addBolus(new BolusDelivery(units, LocalTime.now(), mode));
             if(units > 0 && (mode == BolusMode.STANDARD || mode == BolusMode.EXTENDED))
                 manager.verifyAndInject(units);
             else if (units > 0 && (mode == BolusMode.PEN || mode == BolusMode.MANUAL))
-                addBolus(new BolusDelivery(units, LocalTime.now(), 0, mode));
+                addBolus(new BolusDelivery(units, LocalTime.now(), mode));
                 System.out.println("You should manually inject " + Math.round(units) + " units");
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,7 +53,7 @@ public class LocalDatabase extends Database implements Observer {
 
     private void backup() throws InternetException {
         try {
-            //backupDb.update(); //TODO: update() to implement into BackupDatabase
+            backupDb.update(this.measurements); //TODO: update() to implement into BackupDatabase
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,7 +63,7 @@ public class LocalDatabase extends Database implements Observer {
         updateHourlyFactor(hf);
     }
 
-    public void addBolus(BolusDelivery bolusDelivery) {
-        bolusDeliveries.add(bolusDelivery);
+    public void addBolus(BolusDelivery bd) {
+        bolusDeliveries.add(bd);
     }
 }

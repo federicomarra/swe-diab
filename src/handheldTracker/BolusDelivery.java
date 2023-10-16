02689e6da70cfcf5557f4ec3722badb36fbc692d
federicomarra <fecrom@gmail.com>
2023-10-16 23:41:19 +0200

    calculateResidualUnits() round fixed in BolusDelivery.java

--- src/handheldTracker/BolusDelivery.java
+++ src/handheldTracker/BolusDelivery.java
@@ -22,9 +22,10 @@ public class BolusDelivery {
         do {    // calculate residual units from last 3 hours (linear decay)
             i++;
             BolusDelivery lb = bds.get(bds.size()-i);  // last bolus
-            diff = Duration.between(lb.time, LocalTime.now());
-            residualUnits += lb.units*(1 - (float)diff.toMinutes()/180);
-        } while (diff.toMinutes() <= 180 && i < bds.size());
+            diff = Duration.between(lb.time, LocalTime.now());  // difference between last bolus time and now
+            residualUnits += lb.units*(1 - (float)diff.toMinutes()/180);    // linear decay
+        } while (diff.toMinutes() <= 180 && i < bds.size());    // while last bolus is not older than 3 hours
+        residualUnits = Math.round(residualUnits / 0.01f) * 0.01f; // round to 2 decimal places
         return residualUnits;
     }
 

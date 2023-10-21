5629b2d48c3de0ffba8851a837f60df17f047696
federicomarra <fecrom@gmail.com>
2023-10-21 18:03:58 +0200

    Update UserInterface.java

--- src/handheldTracker/UserInterface.java
+++ src/handheldTracker/UserInterface.java
@@ -1,60 +1,38 @@
 package handheldTracker;
 
 import glucoseDeliverySystem.PumpManager;
-import utils.HourlyFactor;
-
-import java.time.LocalTime;
+import utils.*;
 
 public class UserInterface {
     private LocalDatabase localDb;
 
     public UserInterface() {
-        localDb = new LocalDatabase(new PumpManager());
+        localDb = new LocalDatabase(); //singleton pattern (one localDb for one user)
+    }
+    public void newStandardBolus(int carb) {
+        localDb.newBolus(0, 0, BolusMode.STANDARD, carb);
+    }
+    public void newExtendedBolus(int carb, int delay) {
+        localDb.newBolus(0, delay, BolusMode.EXTENDED, carb);
+    }
+    public void howManyUnits(int carb) {
+        localDb.newBolus(0, 0, BolusMode.MANUAL, carb);
+    }
+    public void newPenBolus(float units) {
+        localDb.newBolus(units, 0, BolusMode.PEN, 0);
     }
 
-    public void newBolus(float units, LocalTime time, int delay, BolusMode mode, int carb) {
-        switch (mode) {
-            case STANDARD:
-                try{
-                    localDb.computeAndInject(carb, mode);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case EXTENDED:
-                try {
-                    wait((long)delay*1000*60);    //delay in minutes
-                    localDb.computeAndInject(carb, mode);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case MANUAL:
-                localDb.addBolus(new BolusDelivery(units, LocalTime.now(), mode));
-                break;
-            case PEN:
-                localDb.addBolus(new BolusDelivery(units, LocalTime.now(), mode));
-                break;
-            default:
-                System.out.println("Invalid bolus mode");
-                break;
-        }
-        //TODO: implement
-        localDb.addBolus(new BolusDelivery(units, time, mode));
-    }
+
 
     public void updateBasalProfile(float units, int hour) {
-        //TODO: implement
-        localDb.updateHourlyFactor(new HourlyFactor(units, hour));
+        localDb.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.BASAL);
     }
 
-    public void updateCarbRatioProfile(float units, int hour) {
-        //TODO: implement
-        //updateHourlyFactor(new HourlyFactor(units, hour));
+    public void updateCarbRatioProfile(int units, int hour) {
+        localDb.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.IC);
     }
 
-    public void updateInsulinSensitivityProfile(float units, int hour) {
-        //TODO: implement
-        //updateHourlyFactor(new HourlyFactor(units, hour));
+    public void updateInsulinSensitivityProfile(int units, int hour) {
+        localDb.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.IG);
     }
 }

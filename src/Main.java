d049db222ffc8a7368a0cfb606a0c18e0e80be23
federicomarra <fecrom@gmail.com>
2023-11-01 18:08:44 +0100

    Randomization of test values in Main.java

--- src/Main.java
+++ src/Main.java
@@ -25,9 +25,12 @@ public class Main {
 
         System.out.println("\n\nStarting simulation:\n");
 
-        int carb = (int) (Math.abs(Math.random() * 300 - 150));
-        int mode = (int) (Math.abs(Math.random() * 3));
-        ui.newPenBolus(1.5f);        // to test calculation of residual units
+        // randomization carb, mode, delay and active insulin
+        int carb = (int) (Math.abs(Math.random() * 300 - 150));     // randomize carb between -150 and 150 put in absolute value
+        int mode = (int) (Math.abs(Math.random() * 3));             // randomize mode between 0 and 2
+        int delay = (int) (Math.abs(Math.random() * 3)+1);          // randomize delay between 1 and 3
+        ui.newPenBolus((float) (Math.abs(Math.random() * 5)));      // randomize residual units (still active insulin)
+
         switch (mode) {
             case 0:
                 System.out.println("Mode: Standard Bolus");
@@ -35,7 +38,7 @@ public class Main {
                 break;
             case 1:
                 System.out.println("Mode: Extended Bolus");
-                ui.newExtendedBolus(carb, 1);
+                ui.newExtendedBolus(carb, delay);
                 break;
             case 2:
                 System.out.println("Mode: Manual Bolus");

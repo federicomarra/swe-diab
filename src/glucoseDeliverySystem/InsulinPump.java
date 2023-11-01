97af5526205411c3ff72e1f784b2cfa9777f3ca6
federicomarra <fecrom@gmail.com>
2023-11-01 18:54:40 +0100

    Update InsulinPump.java

--- src/glucoseDeliverySystem/InsulinPump.java
+++ src/glucoseDeliverySystem/InsulinPump.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class InsulinPump {
     public void inject(float units) {
-        System.out.println("The insulin pump is delivering: " + String.format("%.2f", units) + " units at: " + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")) );
+        System.out.println("The insulin pump is delivering: " + String.format("%.2f", units) + " units at " + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")) );
     }
     //DONE
 }

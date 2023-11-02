cc9b76ffeff4ca4dd4e58fbc50df14e59d5f8269
federicomarra <fecrom@gmail.com>
2023-11-02 22:44:50 +0100

    Method calculateResidualUnits() fixed

--- src/handheldTracker/BolusDelivery.java
+++ src/handheldTracker/BolusDelivery.java
@@ -16,17 +16,22 @@ public class BolusDelivery {
     }
 
     public float calculateResidualUnits(List<BolusDelivery> bds) {  // bds = bolusDeliveries
-        int i = 0;
         int hours = 3;
-        Duration diff;
         float residualUnits = 0;
         if (!bds.isEmpty()) {
-            do {    // calculate residual units from last 3 hours (linear decay)
+            // cycle init
+            int i = 0;
+            BolusDelivery lb = bds.get(bds.size() - 1); // init to last bolus
+            Duration diff = Duration.between(lb.time, LocalTime.now());
+
+            while (diff.toMinutes() <= hours * 60 && i < bds.size()) {   // while last bolus is not older than 3 hours
+                // cycle iteration
+                residualUnits += lb.units * (1 - (float) diff.toMinutes() / (hours * 60));    // linear decay
+                // cycle increment
                 i++;
-                BolusDelivery lb = bds.get(bds.size() - i);  // last bolus
+                lb = bds.get(bds.size() - i);  // last bolus
                 diff = Duration.between(lb.time, LocalTime.now());  // difference between last bolus time and now
-                residualUnits += lb.units * (1 - (float) diff.toMinutes() / 180);    // linear decay
-            } while (diff.toMinutes() <= hours * 60 && i < bds.size());    // while last bolus is not older than 3 hours
+            }
         }
         return residualUnits;
     }

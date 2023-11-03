84b7edc092b3969b06a3d8dcef8f24e2fa6126ae
federicomarra <fecrom@gmail.com>
2023-11-03 13:35:05 +0100

    Update ExampleTest.java

--- src/test/ExampleTest.java
+++ src/test/ExampleTest.java
@@ -10,10 +10,13 @@ public class ExampleTest {
 
     @Test
     public void squareTest() {
+        Assert.assertEquals(100, square(10));
+        Assert.assertEquals(36, square(6));
         Assert.assertEquals(25, square(5));
         Assert.assertEquals(16, square(4));
         Assert.assertEquals(9, square(3));
         Assert.assertEquals(4, square(2));
         Assert.assertEquals(1, square(1));
+        Assert.assertEquals(0, square(0));
     }
 }

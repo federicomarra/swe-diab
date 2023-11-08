b630f464a129a13f28fd596dd000d690822b7573
federicomarra <fecrom@gmail.com>
2023-11-08 17:39:25 +0100

    Update BolusDeliveryTest.java

--- src/test/BolusDeliveryTest.java
+++ src/test/BolusDeliveryTest.java
@@ -14,36 +14,42 @@ public class BolusDeliveryTest {
 
     @Test
     public void ConstructorTest() {
-        // Randomize delay between 1 and 15 with 1 second precision
-        int delay = (int) (Math.abs(Math.random() * 14) + 1);
-
         float[] units = new float[4];
         for (int i = 0; i < 2; i++) // Randomize units between 1 and 15 with 0.01 precision
-            units[i] = (float) ((Math.abs(Math.random() * 1400) + 1) * 0.01);
+            units[i] = (float) (((int)Math.abs(Math.random() * 2900) + 100) * 0.01);
         for (int j = 2; j < 4; j++) // Randomize units between 1 and 15 with 0.5 precision
-            units[j] = (float) ((Math.abs(Math.random() * 140) + 1) * 0.5);
+            units[j] = (float) (((int)Math.abs(Math.random() * 29) + 1) * 0.5);
+        int delay = (int) (Math.abs(Math.random() * 14) + 1); // Randomize delay between 1 and 15 with 1 second precision
 
         // Standard bolus test
         BolusDelivery bd0 = new BolusDelivery(units[0], LocalTime.now(), BolusMode.STANDARD);
         Assert.assertEquals(units[0], bd0.units, 0);
+        Assert.assertEquals(LocalTime.now().getHour(), bd0.time.getHour(), 0.01);
+        Assert.assertEquals(LocalTime.now().getMinute(), bd0.time.getMinute(), 0.01);
         Assert.assertEquals(LocalTime.now().getSecond(), bd0.time.getSecond(), 0.01);
         Assert.assertEquals(BolusMode.STANDARD, bd0.mode);
 
         // Extended bolus test
         BolusDelivery bd1 = new BolusDelivery(units[1], LocalTime.now().plusMinutes(delay), BolusMode.EXTENDED);
         Assert.assertEquals(units[1], bd1.units, 0);
-        Assert.assertEquals(LocalTime.now().plusMinutes(delay).getSecond(), bd1.time.getSecond(), 0.01);
+        Assert.assertEquals(LocalTime.now().getHour(), bd1.time.getHour(), 0.01);
+        Assert.assertEquals(LocalTime.now().plusMinutes(delay).getMinute(), bd1.time.getMinute(), 0.01);
+        Assert.assertEquals(LocalTime.now().getSecond(), bd1.time.getSecond(), 0.01);
         Assert.assertEquals(BolusMode.EXTENDED, bd1.mode);
 
         // Manual bolus test
         BolusDelivery bd2 = new BolusDelivery(units[2], LocalTime.now(), BolusMode.MANUAL);
         Assert.assertEquals(units[2], bd2.units, 0.01);
+        Assert.assertEquals(LocalTime.now().getHour(), bd2.time.getHour(), 0.01);
+        Assert.assertEquals(LocalTime.now().getMinute(), bd2.time.getMinute(), 0.01);
         Assert.assertEquals(LocalTime.now().getSecond(), bd2.time.getSecond(), 0.5);
         Assert.assertEquals(BolusMode.MANUAL, bd2.mode);
 
         // Pen bolus test
         BolusDelivery bd3 = new BolusDelivery(units[3], LocalTime.now(), BolusMode.PEN);
         Assert.assertEquals(units[3], bd3.units, 0.05);
+        Assert.assertEquals(LocalTime.now().getHour(), bd3.time.getHour(), 0.01);
+        Assert.assertEquals(LocalTime.now().getMinute(), bd3.time.getMinute(), 0.01);
         Assert.assertEquals(LocalTime.now().getSecond(), bd3.time.getSecond(), 0.5);
         Assert.assertEquals(BolusMode.PEN, bd3.mode);
     }

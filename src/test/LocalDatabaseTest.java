6969fa6c3d3d9f2ad26c7890eae9719b4fbe26c8
federicomarra <fecrom@gmail.com>
2023-11-08 19:55:45 +0100

    Update LocalDatabaseTest.java

--- src/test/LocalDatabaseTest.java
+++ src/test/LocalDatabaseTest.java
@@ -8,6 +8,8 @@ import handheldTracker.BolusDelivery;
 import handheldTracker.BolusMode;
 import utils.Measurement;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 public class LocalDatabaseTest {
@@ -45,33 +47,54 @@ public class LocalDatabaseTest {
     public void computeAndInjectTest() {
         //computeAndInject() is private so it can be tested using newBolus()
         LocalDatabase db = new LocalDatabase();
-
-        int gref = 120; // Reference glycemia
-
-        int c = (int) (Math.abs(Math.random() * 300 - 150)); // Randomize carb between -150 and 150 put in absolute value
-        int g = (int) (Math.abs(Math.random() * 300 - 160)); // Randomize glycemia between -150 and 150 put in absolute value
-        float csens = db.carbRatioProfile.hourlyFactors[LocalTime.now().getHour()].units(); // Carb sensitivity
-        float isens = db.insulinSensitivityProfile.hourlyFactors[LocalTime.now().getHour()].units();    // Insulin sensitivity
+        // Reference glycemia
+        int gref = 120;
+        // Delay in seconds
+        int delaySeconds = 0;
+        // Randomize carb between -150 and 150 put in absolute value
+        int c = (int) (Math.abs(Math.random() * (150 + 150) - 150));
+        // Randomize glycemia between 160 and 300 put in absolute value
+        int g = (int) (Math.abs(Math.random() * (300 - 160) + 160));
+        // Generate new measurement and add it to the measurements list
+        db.measurements.add(new Measurement(g, LocalTime.now()));
+        // Carb sensitivity
+        float csens = db.carbRatioProfile.hourlyFactors[LocalTime.now().getHour()].units();
+        // Insulin sensitivity
+        float isens = db.insulinSensitivityProfile.hourlyFactors[LocalTime.now().getHour()].units();
+        // Units expected
         float u = (c / csens);
         if (g > gref) u += ((g - gref) / isens);
-        int m = (int) (Math.abs(Math.random() * 3)); // Randomize mode between 0 and 2
-        db.measurements.add(new Measurement(g, LocalTime.now()));
-
-        BolusDelivery bd = new BolusDelivery(u, LocalTime.now(), BolusMode.STANDARD);
-        db.newBolus(0, 0, bd.mode, c);
-        Assert.assertEquals(u, db.bolusDeliveries.get(db.bolusDeliveries.size() - 1).units, 0.1);
+        // Randomization of mode
+        int m = (int) (Math.abs(Math.random() * 3)); // Randomize between 0 and 2
+        BolusMode mode = null;
+        switch (m) {
+            case 0: // Standard bolus test
+                mode = BolusMode.STANDARD;
+                break;
+            case 1: // Extended bolus test
+                mode = BolusMode.EXTENDED;
+                // Randomize delay between 1 and 10 seconds
+                delaySeconds = (int) (Math.abs(Math.random() * (10 - 1)) + 1);
+                System.out.println("Delay: " + delaySeconds + " seconds");
+                break;
+            case 2: // Manual bolus test
+                mode = BolusMode.MANUAL;
+                // Approximation of units to 0.5
+                BigDecimal bigDecimal = new BigDecimal(u);
+                bigDecimal = bigDecimal.setScale(2, RoundingMode.HALF_UP);
+                u = bigDecimal.divide(new BigDecimal("0.5"), 0, RoundingMode.HALF_UP).multiply(new BigDecimal("0.5")).floatValue();
+                break;
+            // Pen bolus case does not exist because it is not possible to call computeAndInject() with Pen mode
+        }
+        BolusDelivery bd = new BolusDelivery(u, LocalTime.now().plusSeconds(delaySeconds), mode);
+        // inside this it is called computeAndInject() which is private
+        db.newBolus(0, delaySeconds, bd.mode, c);
+        Assert.assertEquals(1, db.bolusDeliveries.size());
+        Assert.assertEquals(bd.units, db.bolusDeliveries.get(db.bolusDeliveries.size() - 1).units, 0.01);
         Assert.assertEquals(bd.time.getHour(), db.bolusDeliveries.get(db.bolusDeliveries.size() - 1).time.getHour());
         Assert.assertEquals(bd.time.getMinute(), db.bolusDeliveries.get(db.bolusDeliveries.size() - 1).time.getMinute());
         Assert.assertEquals(bd.time.getSecond(), db.bolusDeliveries.get(db.bolusDeliveries.size() - 1).time.getSecond());
         Assert.assertEquals(bd.mode, db.bolusDeliveries.get(db.bolusDeliveries.size() - 1).mode);
-        Assert.assertEquals(1, db.bolusDeliveries.size());
-
-    /*
-        float u = (float) (((int)Math.abs(Math.random() * 2900) + 100) * 0.01);
-        float ue = (float) (((int)Math.abs(Math.random() * 2900) + 100) * 0.01);
-        float um = (float) (((int)Math.abs(Math.random() * 29) + 1) * 0.5); // Randomize units between 1 and 15 with 0.5 precision
-        int delay = (int) (Math.abs(Math.random() * 14) + 1); // Randomize delay between 1 and 15 with 1 second precision
-        */
     }
 
     @Test

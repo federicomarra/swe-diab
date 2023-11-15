b518bc54ce2286c96718a30c6250fa0945141634
federicomarra <fecrom@gmail.com>
2023-11-15 04:09:28 +0100

    Renamed attribute db

--- src/handheldTracker/UserInterface.java
+++ src/handheldTracker/UserInterface.java
@@ -2,44 +2,42 @@ package handheldTracker;
 
 import utils.*;
 
-import java.util.Scanner;
-
 public class UserInterface {
-    private final LocalDatabase localDb;
+    private final LocalDatabase db;
     // TODO: put this instead?
     // private final LocalDatabase localDb;
 
     public UserInterface() {
         // One localDb for one user
-        localDb = new LocalDatabase();
+        db = new LocalDatabase();
     }
 
     public void newStandardBolus(int carb) {
-        localDb.newBolus(0, 0, BolusMode.STANDARD, carb);
+        db.newBolus(0, 0, BolusMode.STANDARD, carb);
     }
 
     public void newExtendedBolus(int carb, int delayMinutes) { // delay in minutes
-        localDb.newBolus(0, delayMinutes * 60, BolusMode.EXTENDED, carb);  // delay in seconds
+        db.newBolus(0, delayMinutes * 60, BolusMode.EXTENDED, carb);  // delay in seconds
     }
 
     public void howManyUnits(int carb) {
-        localDb.newBolus(0, 0, BolusMode.MANUAL, carb);
+        db.newBolus(0, 0, BolusMode.MANUAL, carb);
     }
 
     public void newPenBolus(float units) {
-        localDb.newBolus(units, 0, BolusMode.PEN, 0);
+        db.newBolus(units, 0, BolusMode.PEN, 0);
     }
 
     public void updateBasalProfile(float units, int hour) {
-        localDb.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.BASAL);
+        db.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.BASAL);
     }
 
     public void updateCarbRatioProfile(float units, int hour) {
-        localDb.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.IC);
+        db.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.IC);
     }
 
     public void updateInsulinSensitivityProfile(float units, int hour) {
-        localDb.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.IG);
+        db.updateHourlyFactor(new HourlyFactor(units, hour), ProfileMode.IG);
     }
 
 }

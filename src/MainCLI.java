6ebde88ca5d9026d7537041e88736b42e6883f2b
federicomarra <fecrom@gmail.com>
2023-12-08 20:16:58 +0100

    Added mode 3 & 4 and fixed safeInput

--- src/MainCLI.java
+++ src/MainCLI.java
@@ -1,5 +1,9 @@
+import handheldTracker.BolusDelivery;
+import handheldTracker.BolusMode;
 import handheldTracker.UserInterface;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class MainCLI {
@@ -9,24 +13,33 @@ public class MainCLI {
         System.out.println("\n\nStarting simulation:\n");
 
         int mode;
+        int selection;
         do {
-            System.out.println("Press 0. to Exit, 1. New bolus, 2. Update profile");
-            mode = (int) safeInput("mode", 0, 2, 1f);
+            selection = 0;
+            System.out.println("0. Exit, 1. New bolus, 2. Update profile, 3. How many units left, 4. Backup");
+            mode = (int) safeInput("mode", 0, 4, 1f);
             if (mode == 1) {
                 System.out.print("Select bolus type: ");
-                System.out.println("0. to Exit, 1. Standard Bolus, 2. Extended Bolus, 3. Manual Bolus, 4. Pen Bolus");
-                mode = (int) safeInput("mode", 0, 4, 1f);
+                System.out.println("0. Exit, 1. Standard Bolus, 2. Extended Bolus, 3. Manual Bolus, 4. Pen Bolus");
+                selection = (int) safeInput("mode", 0, 4, 1f);
             } else if (mode == 2) {
                 System.out.print("Select profile type: ");
                 System.out
-                        .println("0. to Exit, 1. Basal Profile, 2. Carb Ratio Profile, 3. Insulin Sensitivity Profile");
-                mode = (int) safeInput("mode", 0, 3, 1f);
-                mode += (mode == 0 ? 0 : 4);
+                        .println("0. Exit, 1. Basal Profile, 2. Carb Ratio Profile, 3. Insulin Sensitivity Profile");
+                selection = (int) safeInput("mode", 0, 3, 1f);
+            } else if (mode == 3) {
+                BolusDelivery bd = new BolusDelivery(0, null, BolusMode.STANDARD);
+                System.out.print("There is " + bd.calculateResidualUnits(ui.getDb().bolusDeliveries) + " units.\n");
+                selection = 1;
+            } else if (mode == 4) {
+                ui.backup();
+                selection = 1;
             }
-            if (mode >= 1 && mode <= 3) {
-                int carb = (int) safeInput("carbohydrates", 0, 150, 1f);
-
-                switch (mode) {
+            if (mode == 1 && selection >= 1 && selection <= 4) {
+                int carb = 0;
+                if (selection != 4)
+                    carb = (int) safeInput("carbohydrates", 0, 150, 1f);
+                switch (selection) {
                     case 1:
                         ui.newStandardBolus(carb);
                         break;
@@ -37,56 +50,58 @@ public class MainCLI {
                     case 3:
                         ui.howManyUnits(carb);
                         break;
+                    case 4:
+                        float units = safeInput("units", 0, 15, 0.5f);
+                        ui.newPenBolus(units);
+                        break;
                 }
-            } else if (mode == 4) {
-                float units = safeInput("units", 0, 15, 1f);
-                ui.newPenBolus(units);
-            } else if (mode >= 5 && mode <= 7) {
+            } else if (mode == 2 && selection >= 1 && selection <= 3) {
                 int hour = (int) safeInput("hour", 0, 24, 1f);
                 float units;
-
-                switch (mode) {
+                switch (selection) {
                     // Update Basal Profile
-                    case 5:
+                    case 1:
                         units = safeInput("units", 0, 5, 0.05f);
                         ui.updateBasalProfile(units, hour);
                         break;
                     // Update Carb Ratio Profile
-                    case 6:
+                    case 2:
                         units = safeInput("units", 1, 15, 1f);
                         ui.updateCarbRatioProfile(units, hour);
                         break;
                     // Update Insulin Sensitivity Profile
-                    case 7:
+                    case 3:
                         units = safeInput("units", 20, 50, 1f);
                         ui.updateInsulinSensitivityProfile(units, hour);
                         break;
                 }
             }
             System.out.println();
-        } while (mode > 0);
+        } while (selection > 0);
         System.out.println("Exiting...");
     }
 
-    public static float safeInput(String nameVar, int min, int max, float sensitivity) {
+    public static float safeInput(String nameVar, int min, int max, float sensibility) {
         float value;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Insert " + nameVar + ": ");
         while (true) {
-            if (scanner.hasNextFloat()) {
-                value = scanner.nextFloat();
-                if (value < min || value > max || value % sensitivity != 0) {
-                    System.out.print("Insert a number between " + min + " and " + max
-                            + (sensitivity == 1 ? "" : " with a sensitivity of " + sensitivity) + ". Try again: ");
-                } else {
-                    return value;
-                }
-            } else {
+            System.out.print("Insert " + nameVar + ": ");
+            try {
+                value = Float.parseFloat(scanner.next().replace(",", "."));
+            } catch (NumberFormatException e) {
+                value = min - 1;
                 System.out.println("Invalid input. Please enter a valid float number.");
-                // Consume invalid input
-                scanner.next();
-                System.out.print("Insert " + nameVar + ": ");
+            }
+
+            BigDecimal n = new BigDecimal(value).setScale(2, RoundingMode.HALF_UP);
+            float delta = n.remainder(new BigDecimal(sensibility).setScale(2, RoundingMode.HALF_UP)).setScale(2, RoundingMode.HALF_UP).floatValue();
+
+            if (value < min || value > max || delta != 0) {
+                System.out.println("Insert a number between " + min + " and " + max
+                        + (delta != 0 ? " with a sensibility of " + sensibility : "") + ", try again.");
+            } else {
+                return value;
             }
         }
 
